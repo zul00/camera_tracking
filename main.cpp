3406c5860d8d10aef85c2641ed2d280f34ccaaa9
zul00 <zul.zulkarnaen@gmail.com>
2017-06-22 17:24:57 +0000

    Change source to c++, start fresh

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file  main.c
+ * @brief Control two joint JIWY module
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <inttypes.h>
+
+//#include "libuart.h"
+
+int main(int argc, char *argv[])
+{
+  printf("Hello cpp\n");
+
+  for(;;)
+  {
+    printf("Hello 123\n");
+    sleep(1);
+  }
+
+  return 0;
+}
