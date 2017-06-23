ecf1bb53a6f354702c35f1e2d7ce1c88d0ddbdd7
zul00 <zul.zulkarnaen@gmail.com>
2017-06-23 07:55:18 +0000

    Moved two function to vision.cpp

--- vision.hpp
+++ vision.hpp
@@ -4,14 +4,16 @@
 #include <iostream>
 
 void createTrackbars();
+void drawObject(int x, int y, cv::Mat &frame);
+void morphOps(cv::Mat &in, cv::Mat &out);
+void trackFilteredObject(int &x, int &y, cv::Mat threshold, cv::Mat &cameraFeed);
 
-using namespace std;
 
-extern const string windowName;
-extern const string windowName1;
-extern const string windowName2;
-extern const string windowName3;
-extern const string trackbarWindowName;
+extern const std::string windowName;
+extern const std::string windowName1;
+extern const std::string windowName2;
+extern const std::string windowName3;
+extern const std::string trackbarWindowName;
 
 // HSV Filter value
 extern int H_MIN;
